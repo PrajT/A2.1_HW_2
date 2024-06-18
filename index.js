80fbb4e35d1a49f8b784302987337e02b606c8e3
PrajT <105973365+PrajT@users.noreply.github.com>
2024-06-18 15:30:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+console.log("A2.1_HW_1")
+
+console.log("---- ---- ----")
+
+console.log("Exercise 1")
+console.log("---- ---- ----")
+
+console.log("1.1")
+let numOne = 10
+let numTwo = 5
+let numThree = 25
+
+console.log("---- ---- ----")
+
+let sum = numOne + numTwo
+
+console.log("Number One:" , numOne )
+console.log("Number Two:" , numTwo)
+console.log("Sum:" , sum)
+
+console.log("---- ---- ----")
+
+console.log("1.2")
+let difference = numOne - numTwo
+
+console.log("Number One:" , numOne )
+console.log("Number Two:" , numTwo)
+console.log("difference:" , difference)
+
+console.log("---- ---- ----")
+
+console.log("1.3")
+let product = numOne * numTwo
+
+console.log("Number One:" , numOne )
+console.log("Number Two:" , numTwo)
+console.log("product:" , product)
+
+console.log("---- ---- ----")
+
+console.log("1.4")
+let division = numOne / numTwo
+
+console.log("Number One:" , numOne)
+console.log("Number Two:" , numTwo)
+console.log("division:" , division)
+
+console.log("---- ---- ----")
+
+console.log("1.5")
+let reminder = numOne % numTwo
+
+console.log("Number One:" , numOne)
+console.log("Number Two:" , numTwo)
+console.log("Remainder:" , reminder)
+
+console.log("---- ---- ----")
+console.log("Exercise 2")
+console.log("---- ---- ----")
+
+console.log("2.1")
+let sumAll = numOne + numTwo + numThree
+console.log("Number One:" , numOne)
+console.log("Number Two:" , numTwo)
+console.log("Number Three:" , numThree)
+console.log("Sum:" , sumAll)
+
+console.log("---- ---- ----")
+
+console.log("2.2")
+let sumAndSubtract = numThree - (numOne + numTwo)
+console.log("Number One:" , numOne )
+console.log("Number Two:" , numTwo)
+console.log("Number Three:" , numThree)
+console.log("Sum and Subtract:" , sumAndSubtract)
+
+console.log("---- ---- ----")
+
+console.log("2.3")
+let productAndAdd = (numOne * numTwo) + numThree
+console.log("Number One:" , numOne)
+console.log("Number Two:" , numTwo)
+console.log("Number Three:" , numThree)
+console.log("Product and Add:" , productAndAdd)
+
+console.log("---- ---- ----")
+
+console.log("2.4")
+let divideAndAdd = (numThree / numOne) + numTwo
+console.log("Number One:" , numOne)
+console.log("Number Two:" , numTwo)
+console.log("Number Three:" , numThree)
+console.log("Divide and Add:" , divideAndAdd)
+
+console.log("---- ---- ----")
+console.log("2.5")
+let average = (numOne + numTwo + numThree) / 3
+console.log("Number One:" , numOne)
+console.log("Number Two:" , numTwo)
+console.log("Number Three:" , numThree)
+console.log("Average:" , average)
